b8d0468a90f13e5b3acb6db7eb3cd7c4dcf2e55e
KHMYROV Denis <DEK2696@uni.rest>
2023-10-26 22:44:29 +0300

    code refactored due to apply styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { get } from './api';
-import GetOrderButton from './components/getOrderButton';
-import OrderList from './components/productList';
-import ProductImages from './components/productsImages';
+import Sidebar from './components/Sidebar';
 
 interface OrderItem {
   name: string;
@@ -14,27 +11,14 @@ function App() {
   const [data, setData] = useState<OrderItem[] | null>(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const result = await get('http://localhost:3500/get_random_order');
-      setData(result);
-    } catch (error) {
-      console.error('Ошибка при запросе данных:', error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   return (
     <div>
-      <GetOrderButton onClick={fetchData} />
+      <Sidebar data={data} setData={setData} setLoading={setLoading} />
       {loading ? (
         <p>Получение заказа...</p>
       ) : data ? (
         <div>
-          <OrderList data={data} />
-          <ProductImages images={data.map(item => item.img)} />
+          {/* Остальная часть вашего компонента */}
         </div>
       ) : (
         <p>Заказ отсутствует</p>
--- src/api.ts
+++ src/api.ts
@@ -1,15 +1,31 @@
-import URL from './const.ts';
 import axios from 'axios';
+import URL from './const.ts';
 
 const api = axios.create({
   baseURL: URL.getRandomOrder,
 });
 
 export const get = async (url: string) => {
-    const response = await api.get(url);
-
-    return response.data;
+  const response = await api.get(url);
+  return response.data;
 }
 
+export interface OrderItem {
+  name: string;
+  img: string;
+}
 
-export default api;
+export const fetchData = async (
+  setData: React.Dispatch<React.SetStateAction<OrderItem[] | null>>,
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  try {
+    setLoading(true);
+    const result = await get('http://localhost:3500/get_random_order');
+    setData(result);
+  } catch (error) {
+    console.error('Ошибка при запросе данных:', error);
+  } finally {
+    setLoading(false);
+  }
+}
